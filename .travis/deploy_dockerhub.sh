227ffc968f7c24be56a4fe85aeb880444f00ce21
thomasbui198 <notnewtothis12@gmail.com>
2020-02-24 20:20:28 +0000

    Modify deploy_dockerhub.sh to push to Docker using Travis

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo $DOCKER_USER
 echo $DOCKER_PASS
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p "$DOCKER_PASS"
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
